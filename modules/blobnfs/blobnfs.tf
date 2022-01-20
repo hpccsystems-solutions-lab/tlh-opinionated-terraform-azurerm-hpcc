00cdb9af0a68dc03a3c7b4253017fa268b57144a
Suraj Shetty <suraj.shetty@lexisnexisrisk.com>
2022-01-20 17:23:06 +0530

    Making changes in the module to handle multiple HPCC cluster creation

--- modules/blobnfs/blobnfs.tf
+++ modules/blobnfs/blobnfs.tf
@@ -41,6 +41,9 @@ resource "azurerm_storage_container" "hpcc_storage_containers" {
   storage_account_name  = azurerm_storage_account.storage_account[0].name
   container_access_type = "private"
 }
+
+
+// Removing this resource block as the lock was creating an issue during terraform re-deployment
 /*
 resource "azurerm_management_lock" "protect_storage_account" {
   count      = var.storage_account_delete_protection ? 1 : 0
