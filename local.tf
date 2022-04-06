30907de97278c48eb07cb80e74163aa2e591ea3f
dutsmiller@gmail.com <tim.miller@risk.lexisnexis.com>
2022-04-06 15:26:50 -0400

    bridge commit before refactor merge

---
+++ local.tf
@@ -0,0 +1,281 @@
+locals {
+  namespaces = merge({ hpcc = var.namespace }, var.install_blob_csi_driver ? { csi_driver = local.blob_csi_driver.namespace } : {})
+
+  create_registry_auth_secret = var.container_registry.username != null && var.container_registry.username != "" ? true : false
+
+  internal_data_config = var.data_storage_config.internal == null ? false : true
+  external_data_config = var.data_storage_config.external == null ? false : true
+
+  create_data_storage = (local.internal_data_config ? (var.data_storage_config.internal.blob_nfs == null ? false : true) : false)
+  create_data_cache   = (local.internal_data_config ? (var.data_storage_config.internal.hpc_cache == null ? false : true) : false)
+
+  external_data_storage = (local.external_data_config ? (var.data_storage_config.external.blob_nfs == null ? false : true) : false)
+  external_data_cache   = (local.external_data_config ? (var.data_storage_config.external.hpc_cache == null ? false : true) : false)
+  external_hpcc_data    = (local.external_data_config ? (var.data_storage_config.external.hpcc == null ? false : true) : false)
+
+  storage_config = {
+    blob_nfs = (local.create_data_storage ? module.data_storage.0.data_planes : (
+    local.external_data_storage ? var.data_storage_config.external.blob_nfs : null))
+    hpc_cache = (local.create_data_cache ? module.data_cache.0.data_planes.default : (
+    local.external_data_cache ? var.data_storage_config.external.hpc_cache : null))
+    hpcc = local.external_hpcc_data ? var.data_storage_config.external.hpcc : []
+  }
+
+  blob_nfs_data_enabled  = local.storage_config.blob_nfs == null ? false : true
+  hpc_cache_data_enabled = local.storage_config.hpc_cache == null ? false : true
+  remote_data_enabled    = local.storage_config.hpcc == null ? false : true
+  spill_space_enabled    = var.spill_volume_size == null ? false : true
+
+  blob_nfs_data_storage = local.blob_nfs_data_enabled ? { for plane in local.storage_config.blob_nfs :
+    "data-${plane.id}" => {
+      category        = "data"
+      container_name  = plane.container_name
+      id              = plane.id
+      path            = "hpcc-data"
+      resource_group  = plane.resource_group_name
+      storage_account = plane.storage_account_name
+      size            = "5Pi"
+    }
+  } : {}
+
+  hpc_cache_data_storage = local.hpc_cache_data_enabled ? { for plane in local.storage_config.hpc_cache :
+    "data-${plane.id}" => {
+      name   = "hpc-cache-data-${plane.id}"
+      server = plane.server
+      path   = plane.path
+      size   = "5Pi"
+    }
+  } : {}
+
+  services_storage_config = [
+    {
+      category       = "dali"
+      container_name = "hpcc-dali"
+      path           = "dalistorage"
+      plane_name     = "dali"
+      size           = var.services_storage_size.dali
+    },
+    {
+      category       = "debug"
+      container_name = "hpcc-debug"
+      path           = "debug"
+      plane_name     = "debug"
+      size           = var.services_storage_size.debug
+    },
+    {
+      category       = "dll"
+      container_name = "hpcc-dll"
+      path           = "queries"
+      plane_name     = "dll"
+      size           = var.services_storage_size.dll
+    },
+    {
+      category       = "lz"
+      container_name = "hpcc-mydropzone"
+      path           = "mydropzone"
+      plane_name     = "mydropzone"
+      size           = var.services_storage_size.lz
+    },
+    {
+      category       = "sasha"
+      container_name = "hpcc-sasha"
+      path           = "sashastorage"
+      plane_name     = "sasha"
+      size           = var.services_storage_size.sasha
+    }
+  ]
+
+  blob_nfs_services_storage = { for config in local.services_storage_config :
+    config.plane_name => {
+      category        = config.category
+      container_name  = config.container_name
+      path            = config.path
+      resource_group  = var.resource_group_name
+      size            = config.size
+      storage_account = azurerm_storage_account.services.name
+    }
+  }
+
+  blob_csi_driver = {
+    chart_version = "v1.9.0"
+    namespace = {
+      name = "blob-csi-driver"
+      labels = {
+        name = "blob-csi-driver"
+      }
+    }
+  }
+
+  helm_chart_values = {
+
+    global = {
+      image = merge({
+        version          = var.helm_chart_version
+        root             = var.container_registry.image_root
+        name             = var.container_registry.image_name
+        pullPolicy       = "IfNotPresent"
+        imagePullSecrets = local.create_registry_auth_secret ? kubernetes_secret.container_registry_auth.0.metadata.0.name : null
+        },
+      )
+      visibilities = {
+        cluster = {
+          type = "ClusterIP"
+        }
+        local = {
+          annotations = {
+            "helm.sh/resource-policy"                                 = "keep"
+            "service.beta.kubernetes.io/azure-load-balancer-internal" = "true"
+          }
+          type    = "LoadBalancer"
+          ingress = []
+        }
+        global = {
+          type    = "LoadBalancer"
+          ingress = []
+        }
+      }
+    }
+
+    storage = merge({
+      planes = concat([for k, v in local.blob_nfs_services_storage :
+        {
+          category = v.category
+          name     = k
+          prefix   = "/var/lib/HPCCSystems/${v.path}"
+          pvc      = kubernetes_persistent_volume_claim.blob_nfs[k].metadata.0.name
+        }
+        ], local.blob_nfs_data_enabled ? [
+        merge({
+          category   = "data"
+          name       = "data"
+          numDevices = length(local.blob_nfs_data_storage)
+          prefix     = "/var/lib/HPCCSystems/hpcc-data"
+          pvc        = "pvc-blob-data"
+          }, local.hpc_cache_data_enabled ? {
+          aliases = [
+            {
+              mode      = [ "random" ]
+              name      = "data-cache"
+              numMounts = length(local.hpc_cache_data_storage)
+              prefix    = "/var/lib/HPCCSystems/hpcc-data-cache"
+              pvc       = "pvc-hpc-cache-data"
+            }
+          ]
+        } : {})] : [], (local.hpc_cache_data_enabled && !local.blob_nfs_data_enabled) ? [
+        {
+          category   = "data"
+          name       = "data"
+          numDevices = length(local.hpc_cache_data_storage)
+          prefix     = "/var/lib/HPCCSystems/hpcc-data"
+          pvc        = "pvc-hpc-cache-data"
+        }
+        ] : [], local.spill_space_enabled ? [
+        {
+          category         = "spill"
+          name             = "localspill"
+          prefix           = "/var/lib/HPCCSystems/spill"
+          pvc              = "pvc-spill"
+          forcePermissions = true
+        }
+        ] : []
+      ) }, local.external_hpcc_data ? { remote = local.storage_config.hpcc } : {}
+    )
+
+    certificates = {
+      enabled = false
+      issuers = {
+        local = {
+          name = "letsencrypt-issuer"
+          kind = "ClusterIssuer"
+          spec = null
+        }
+      }
+    }
+
+    eclagent = [
+      {
+        name      = "hthor"
+        replicas  = 1
+        maxActive = 4
+      },
+      {
+        name      = "roxie-workunit"
+        replicas  = 1
+        maxActive = 4
+      }
+    ]
+
+    eclccserver = [
+      {
+        name      = "myeclccserver"
+        replicas  = 1
+        maxActive = 4
+      }
+    ]
+
+    esp = [
+      {
+        name        = "eclwatch"
+        application = "eclwatch"
+        auth        = "none"
+        replicas    = 1
+        service = {
+          port        = 8888
+          servicePort = 8010
+          visibility  = "local"
+        }
+      },
+      {
+        name        = "eclservices"
+        application = "eclservices"
+        auth        = "none"
+        replicas    = 1
+        service = {
+          servicePort = 8010
+          visibility  = "cluster"
+        }
+      },
+      {
+        name        = "eclqueries"
+        application = "eclqueries"
+        auth        = "none"
+        replicas    = 1
+        service = {
+          servicePort = 8002
+          visibility  = "local"
+        }
+      },
+      {
+        name        = "esdl-sandbox"
+        application = "esdl-sandbox"
+        auth        = "none"
+        replicas    = 1
+        service = {
+          servicePort = 8899
+          visibility  = "local"
+        }
+      },
+      {
+        name        = "sql2ecl"
+        application = "sql2ecl"
+        auth        = "none"
+        replicas    = 1
+        service = {
+          servicePort = 8510
+          visibility  = "local"
+        }
+      }
+    ]
+
+    roxie = var.roxie_config
+
+    thor = var.thor_config
+
+    eclscheduler = [
+      {
+        name = "eclscheduler"
+      }
+    ]
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -1,135 +1,277 @@
-variable "location" {
-  description = "Azure region in which to build resources."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the Resource Group to deploy the AKS cluster service to, must already exist."
-  type        = string
-}
-
-variable "blob-csi-driver" {
-  description = "Determines if the blob-csi-drivers are to be installed for the cluster."
-  type        = bool
-  default     = true
-}
-
-
-variable "tags" {
-  description = "Tags to be applied to cloud resources."
-  type        = map(string)
-  default     = {}
-}
-
-# HPCC Storage Config
-
-variable "storage_account_authorized_ip_ranges" {
-  description = "Map of authorized CIDRs / IPs"
-  type        = map(string)
-}
-
-variable "storage_account_delete_protection" {
-  description = "Protect storage from deletion"
-  type        = bool
-  default     = true
-}
-
-variable "service_endpoints" {
-  description = "Creates a virtual network rule in the subnet_id (values are virtual network subnet ids)."
-  type        = map(string)
-  default     = {}
-}
-
-# HPCC Config
-variable "hpcc_helm_version" {
-  description = "Version of the HPCC Helm Chart to use"
-  type        = string
-  default     = "8.6.0"
-}
-
-variable "hpcc_namespace" {
-  description = "HPCC Namespace"
-  type        = string
-  default     = "hpcc"
-}
-
-variable "hpcc_storage_account_name" {
-  description = "Storage account name for hpcc"
-  type        = string
-  default     = ""
-}
-
-variable "hpcc_storage_account_resource_group_name" {
-  description = "Storage account resource group name for hpcc"
-  type        = string
-  default     = ""
-}
-
-variable "hpcc_storage_config" {
-  description = "Storage config for hpcc"
-  type = map(object({
-    container_name = string
-    size           = string
-    })
-  )
-}
-
-variable "hpcc_replica_config" {
-  description = "HPCC component scaling"
-  type        = map(number)
-  default     = {}
-}
-
-/* Future feature
-variable "hpcc_disabled_services" {
-  description = "HPCC disable services"
-  type = map(bool)
-  default = {}
-}
-*/
-
-variable "aks_principal_id" {
-  description = "AKS Principal ID"
-  type        = string
-}
-
-# JFrog Registry
-variable "jfrog_registry" {
-  description = "values to set as secrets for JFrog repo access"
+variable "container_registry" {
+  description = "Registry info for HPCC containers."
   type = object({
-    username   = string
-    password   = string # API Token
-    image_root = string
     image_name = string
+    image_root = string
+    password   = string
+    username   = string
   })
   sensitive = true
 }
 
-variable "hpc_cache_enabled" {
-  description = "Creates the hpc-cache for the cluster."
+variable "data_storage_config" {
+  description = "Data plane config for HPCC."
+  type = object({
+    internal = object({
+      blob_nfs = object({
+        data_plane_count = number
+        storage_account_settings = object({
+          authorized_ip_ranges = map(string)
+          delete_protection    = bool
+          replication_type     = string
+          subnet_ids           = map(string)
+        })
+      })
+      hpc_cache = object({
+        dns = object({
+          zone_name                = string
+          zone_resource_group_name = string
+        })
+        resource_provider_object_id = string
+        size                        = string
+        storage_targets = map(object({
+          cache_update_frequency = string
+          storage_account_data_planes = list(object({
+            container_id         = string
+            container_name       = string
+            id                   = number
+            resource_group_name  = string
+            storage_account_id   = string
+            storage_account_name = string
+          }))
+        }))
+        subnet_id                   = string
+      })
+    })
+    external = object({
+      blob_nfs = list(object({
+        container_id         = string
+        container_name       = string
+        id                   = string
+        resource_group_name  = string
+        storage_account_id   = string
+        storage_account_name = string
+      }))
+      hpc_cache = list(object({
+        id     = string
+        path   = string
+        server = string
+      }))
+      hpcc = list(object({
+        name    = string
+        planes = list(object({
+          local  = string
+          remote = string
+        }))
+        service = string
+      }))
+    })
+  })
+  default = {
+    internal = {
+      blob_nfs = {
+        data_plane_count = 1
+        storage_account_settings = {
+          authorized_ip_ranges = {}
+          delete_protection    = false
+          replication_type     = "LRS"
+          subnet_ids           = {}
+        }
+      }
+      hpc_cache = null
+    }
+    external = null
+  }
+}
+
+variable "enable_node_tuning" {
+  description = "Enable node tuning daemonset (only needed once per AKS cluster)."
   type        = bool
+  default     = true
 }
 
-variable "hpc_cache_dns_name" {
+variable "helm_chart_overrides" {
+  description = "Helm chart values, in yaml format, to be merged last."
+  type        = string
+  default     = ""
+}
+
+variable "helm_chart_version" {
+  description = "Version of the HPCC Helm Chart to use."
+  type        = string
+  default     = "8.6.10-rc1"
+}
+
+variable "install_blob_csi_driver" {
+  description = "Install blob-csi-drivers on the cluster."
+  type        = bool
+  default     = true
+}
+
+variable "location" {
+  description = "Azure region in which to create resources."
+  type        = string
+}
+
+variable "namespace" {
+  description = "Kubernetes namespace where resources will be created."
   type = object({
-    zone_name                = string
-    zone_resource_group_name = string
+    name   = string
+    labels = map(string)
   })
+  default = {
+    name = "hpcc"
+    labels = {
+      name = "hpcc"
+    }
+  }
 }
 
-variable "hpc_cache_name" {
-  type = string
+variable "resource_group_name" {
+  description = "The name of the resource group to deploy resources."
+  type        = string
 }
 
-variable "thor_workers" {
-  description = "Number of thor workers to define the limits for each pod"
-  type = number
+variable "roxie_config" {
+  description = "Configuration for Roxie(s)."
+  type = list(object({
+    name     = string
+    disabled = bool
+    prefix   = string
+    services = list(object({
+      name        = string
+      servicePort = number
+      listenQueue = number
+      numThreads  = number
+      visibility  = string
+    }))
+    replicas       = number
+    numChannels    = number
+    serverReplicas = number
+    topoServer = object({
+      replicas = number
+    })
+  }))
+  default = [
+    {
+      name     = "roxie"
+      disabled = true
+      prefix   = "roxie"
+      services = [
+        {
+          name        = "roxie"
+          servicePort = 9876
+          listenQueue = 200
+          numThreads  = 30
+          visibility  = "local"
+        }
+      ]
+      replicas       = 2
+      numChannels    = 2
+      serverReplicas = 0
+      topoServer = {
+        replicas = 1
+      }
+    }
+  ]
 }
 
-variable "thor_maxvalues" {
-  description = "Thor workers resources limits for each pod"
+variable "services_storage_account_settings" {
+  description = "Settings for internal storage account (if external account is not used)."
   type = object({
-    maxJobs   = number
-    maxGraphs = number
+    authorized_ip_ranges = map(string)
+    delete_protection    = bool
+    replication_type     = string
+    subnet_ids           = map(string)
   })
+  default = {
+    authorized_ip_ranges = {}
+    delete_protection    = false
+    replication_type     = "LRS"
+    subnet_ids           = {}
+  }
+}
+
+variable "services_storage_size" {
+  description = "Storage config for hpcc non-data services."
+  type = object({
+    dali  = string
+    debug = string
+    dll = string
+    lz = string
+    sasha = string
+  })
+  default = {
+    dali  = "100Gi"
+    debug = "100Gi"
+    dll = "100Gi"
+    lz = "1Pi"
+    sasha = "100Gi"
+  }
+}
+
+variable "spill_volume_size" {
+  description = "Size of spill volume to be created."
+  type        = string
+  default     = null
+}
+
+variable "thor_config" {
+  description = "Configuration for Thor(s)."
+  type = list(object({
+    disabled         = bool
+    eclAgentResources = object({
+      cpu    = string
+      memory = string
+    })
+    managerResources = object({
+      cpu    = string
+      memory = string
+    })
+    maxGraphs        = number
+    maxJobs          = number
+    name             = string
+    prefix           = string
+    numWorkers       = number
+    numWorkersPerPod = number
+    workerMemory = object({
+      query      = string
+      thirdParty = string
+    })
+    workerResources = object({
+      cpu    = string
+      memory = string
+    })
+  }))
+  default = [{
+    disabled         = true
+    eclAgentResources = {
+      cpu    = 1
+      memory = "2G"
+    }
+    managerResources = {
+      cpu    = 1
+      memory = "2G"
+    }
+    maxGraphs        = 2
+    maxJobs          = 4
+    name             = "thor"
+    numWorkers       = 2
+    numWorkersPerPod = 1
+    prefix           = "thor"
+    workerMemory = {
+      query      = "3G"
+      thirdParty = "500M"
+    }
+    workerResources = {
+      cpu    = 3
+      memory = "4G"
+    }
+  }]
+}
+
+variable "tags" {
+  description = "Tags to be applied to Azure resources."
+  type        = map(string)
+  default     = {}
 }
