890cfbe58d50a6bf93aa0fa57aeb856e8911fad6
dutsmiller@gmail.com <tim.miller@risk.lexisnexis.com>
2022-04-16 17:36:42 -0400

    simplify registry auth credential

--- registry.tf
+++ registry.tf
@@ -1,9 +1,9 @@
-resource "kubernetes_secret" "container_registry_auth" {
+resource "kubernetes_secret" "hpcc_container_registry_auth" {
   depends_on = [
     kubernetes_namespace.default
   ]
 
-  count = local.create_registry_auth_secret ? 1 : 0
+  count = local.create_hpcc_registry_auth_secret ? 1 : 0
 
   metadata {
     name      = "container-registry-auth"
@@ -13,17 +13,13 @@ resource "kubernetes_secret" "container_registry_auth" {
     }
   }
   data = {
-    ".dockerconfigjson" = <<DOCKER
-      { 
-        "auths" :{ 
-          "${var.container_registry.image_root}": {
-              "username":"${var.container_registry.username}",
-              "password":"${var.container_registry.password}",
-              "auth": "${base64encode("${var.container_registry.username}:${var.container_registry.password}")}"
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.hpcc_container.image_root}" = {
+          auth = base64encode("${var.hpcc_container_registry_auth.username}:${var.hpcc_container_registry_auth.password}")
+        }
       }
-   }
-}
-DOCKER
+    })
   }
   type = "kubernetes.io/dockerconfigjson"
 }
