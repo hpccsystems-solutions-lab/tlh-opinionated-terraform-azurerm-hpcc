f98b99f4380dd1f948aabe3459d3d3f6697a159c
Gianchandani <GianMi01@risk.regn.net>
2022-07-12 18:27:24 -0400

    Outputting Helm Templates

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
-output "local_storage_values" {
-  value = yamlencode(local.helm_chart_values.storage)
+output "mariadb_instance_manifest_bundle" {
+  value = data.helm_template.hpcc.manifest_bundle
+}
+
+output "mariadb_instance_manifests" {
+  value = data.helm_template.hpcc.manifests
+}
+
+output "mariadb_instance_notes" {
+  value = data.helm_template.hpcc.notes
 }
