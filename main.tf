e3a4a700107d76c9033c6abfccd9e58f1116a666
Gianchandani <GianMi01@risk.regn.net>
2022-07-12 23:47:04 -0400

    Updated Ingress to look as expected - An Array

--- main.tf
+++ main.tf
@@ -18,31 +18,6 @@ module "node_tuning" {
 
 }
 
-data "helm_template" "hpcc" {
-  depends_on = [
-    kubernetes_namespace.default,
-    kubernetes_persistent_volume_claim.azurefiles,
-    kubernetes_persistent_volume_claim.blob_nfs,
-    kubernetes_persistent_volume_claim.hpc_cache,
-    kubernetes_persistent_volume_claim.spill,
-    kubernetes_secret.hpcc_container_registry_auth,
-    kubernetes_secret.dali_hpcc_admin,
-    kubernetes_secret.dali_ldap_admin,
-    kubernetes_secret.esp_ldap_admin,
-    module.node_tuning
-  ]
-  
-  timeout = var.helm_chart_timeout
-
-  name       = "hpcc"
-  namespace  = var.namespace.name
-  chart      = "hpcc"
-  repository = "https://hpcc-systems.github.io/helm-chart"
-  version    = var.helm_chart_version
-  values = [
-    yamlencode(local.helm_chart_values)
-  ]
-}
 
 resource "helm_release" "hpcc" {
   depends_on = [
@@ -55,8 +30,7 @@ resource "helm_release" "hpcc" {
     kubernetes_secret.dali_hpcc_admin,
     kubernetes_secret.dali_ldap_admin,
     kubernetes_secret.esp_ldap_admin,
-    module.node_tuning,
-    data.helm_template.hpcc
+    module.node_tuning
   ]
 
   timeout = var.helm_chart_timeout
@@ -67,6 +41,7 @@ resource "helm_release" "hpcc" {
   repository = "https://hpcc-systems.github.io/helm-chart"
   version    = var.helm_chart_version
   values = [
-    yamlencode(local.helm_chart_values)
+    yamlencode(local.helm_chart_values),
+    var.helm_chart_overrides
   ]
 }
--- output.tf
+++
@@ -1,8 +0,0 @@
-
-output "mariadb_instance_manifests" {
-  value = data.helm_template.hpcc.manifests
-}
-
-output "mariadb_instance_notes" {
-  value = data.helm_template.hpcc.notes
-}
