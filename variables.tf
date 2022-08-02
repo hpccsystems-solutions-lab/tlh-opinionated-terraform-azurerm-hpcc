d4371098bbd16a3bde9cbe617aee6171c904a82d
Sadineni <SadiKa01@risk.regn.net>
2022-08-02 13:50:49 -0700

    adding sasha configuration

--- variables.tf
+++ variables.tf
@@ -737,7 +737,7 @@ variable "eclccserver_settings" {
 }
 
 variable "dali_settings" {
-  description = "Set cpu and memory values of the eclccserver. Toggle use_child_process to true to enable eclccserver child processes."
+  description = "dali settings"
   type = object({
     coalescer = object({
       interval     = number
@@ -768,4 +768,108 @@ variable "dali_settings" {
       memory = "8G"
     }
   }
+}
+
+###sasha config
+
+variable "sasha_config" {
+  description = "Configuration for Sasha."
+  type = list(object({
+    disabled = bool
+    wu-archiver = object({
+      disabled = bool
+      service = object({
+        servicePort = number
+      })
+      plane           = string
+      interval        = number
+      limit           = number
+      cutoff          = number
+      backup          = number
+      at              = string
+      throttle        = number
+      retryinterval   = number
+      keepResultFiles = bool
+    })
+
+    dfuwu-archiver = object({
+      disabled = bool
+      service = object({
+        servicePort = number
+      })
+      plane    = string
+      interval = number
+      limit    = number
+      cutoff   = number
+      at       = string
+      throttle = number
+      duration = number
+    })
+
+    dfurecovery-archiver = object({
+      disabled = bool
+      interval = number
+      limit    = number
+      cutoff   = number
+      at       = string
+    })
+
+    file-expiry = object({
+      disabled             = bool
+      interval             = number
+      at                   = string
+      persistExpiryDefault = number
+      expiryDefault        = number
+      user                 = string
+    })
+  }))
+  default = [{
+    disabled = true
+    wu-archiver = {
+      disabled = false
+      service = {
+        servicePort = 8877
+      }
+      plane           = "sasha"
+      interval        = 6
+      limit           = 400
+      cutoff          = 3
+      backup          = 0
+      at              = "* * * * *"
+      throttle        = 0
+      retryinterval   = 6
+      keepResultFiles = false
+    }
+
+    dfuwu-archiver = {
+      disabled = false
+      service = {
+        servicePort = 8877
+      }
+      plane    = "sasha"
+      interval = 24
+      limit    = 100
+      cutoff   = 14
+      at       = "* * * * *"
+      throttle = 0
+      duration = 0
+    }
+
+    dfurecovery-archiver = {
+      disabled = false
+      interval = 12
+      limit    = 20
+      cutoff   = 4
+      at       = "* * * * *"
+    }
+
+    file-expiry = {
+      disabled             = true
+      interval             = 1
+      at                   = "* * * * *"
+      persistExpiryDefault = 7
+      expiryDefault        = 4
+      user                 = "sasha"
+    }
+  }]
 }
