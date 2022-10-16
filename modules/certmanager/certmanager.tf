168078dd94daf3f9d6396683acf0f502aab62eca
Sadineni <SadiKa01@risk.regn.net>
2022-10-16 12:49:07 -0700

    reverting few changes

--- modules/certmanager/certmanager.tf
+++ modules/certmanager/certmanager.tf
@@ -1,63 +1,63 @@
 ###local_issuer####
 ######
 
-# resource "kubernetes_manifest" "local_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/issuer.yml",
-#     {
-#       "name" = "hpcc-local-issuer"
-#     }
-#   ))
-# }
-
-# resource "kubernetes_manifest" "local_cert_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/certificate-issuer.yml",
-#     {
-#       "name"       = "hpcc-local-issuer"
-#       "secretName" = "hpcc-local-issuer-key-pair"
-#       "dnsNames"   = var.internal_domain
-#     }
-#   ))
-
-#   depends_on = [kubernetes_manifest.local_issuer]
-# }
-
-resource "null_resource" "local_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/local/issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "local_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/issuer.yml",
+    {
+      "name" = "hpcc-local-issuer"
     }
-  }
-  # depends_on = [kubernetes_manifest.local_cert_issuer]
+  ))
 }
 
-resource "null_resource" "local_cert_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/local/certificate-issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "local_cert_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/certificate-issuer.yml",
+    {
+      "name"       = "hpcc-local-issuer"
+      "secretName" = "hpcc-local-issuer-key-pair"
+      "dnsNames"   = var.internal_domain
     }
-  }
-  depends_on = [null_resource.local_issuer]
+  ))
+
+  depends_on = [kubernetes_manifest.local_issuer]
 }
+
+# resource "null_resource" "local_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/local/issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   # depends_on = [kubernetes_manifest.local_cert_issuer]
+# }
+
+# resource "null_resource" "local_cert_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/local/certificate-issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   depends_on = [null_resource.local_issuer]
+# }
 resource "null_resource" "local_ca_issuer" {
   provisioner "local-exec" {
     command = <<EOF
@@ -73,68 +73,68 @@ resource "null_resource" "local_ca_issuer" {
       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
     }
   }
-  #depends_on = [kubernetes_manifest.local_cert_issuer]
-  depends_on = [null_resource.local_cert_issuer]
+  depends_on = [kubernetes_manifest.local_cert_issuer]
+  #depends_on = [null_resource.local_cert_issuer]
 }
 
 ###############remote########################
 
-# resource "kubernetes_manifest" "remote_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/issuer.yml",
-#     {
-#       "name" = "hpcc-remote-issuer"
-#     }
-#   ))
-# }
-
-# resource "kubernetes_manifest" "remote_cert_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/certificate-issuer.yml",
-#     {
-#       "name"       = "hpcc-remote-issuer"
-#       "secretName" = "hpcc-remote-issuer-key-pair"
-#       "dnsNames"   = var.internal_domain
-#     }
-#   ))
-#   depends_on = [kubernetes_manifest.remote_issuer]
-# }
-
-resource "null_resource" "remote_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/remote/issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "remote_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/issuer.yml",
+    {
+      "name" = "hpcc-remote-issuer"
     }
-  }
-  # depends_on = [kubernetes_manifest.local_cert_issuer]
+  ))
 }
 
-resource "null_resource" "remote_cert_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/remote/certificate-issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "remote_cert_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/certificate-issuer.yml",
+    {
+      "name"       = "hpcc-remote-issuer"
+      "secretName" = "hpcc-remote-issuer-key-pair"
+      "dnsNames"   = var.internal_domain
     }
-  }
-  depends_on = [null_resource.remote_issuer]
+  ))
+  depends_on = [kubernetes_manifest.remote_issuer]
 }
+
+# resource "null_resource" "remote_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/remote/issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   # depends_on = [kubernetes_manifest.local_cert_issuer]
+# }
+
+# resource "null_resource" "remote_cert_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/remote/certificate-issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   depends_on = [null_resource.remote_issuer]
+# }
 
 resource "null_resource" "remote_ca_issuer" {
   provisioner "local-exec" {
@@ -147,68 +147,69 @@ resource "null_resource" "remote_ca_issuer" {
       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
     }
   }
-  depends_on = [null_resource.remote_cert_issuer]
+ # depends_on = [null_resource.remote_cert_issuer]
+ depends_on = [kubernetes_manifest.remote_cert_issuer]
 }
 
 ###################signing#################
 
-# resource "kubernetes_manifest" "signing_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/issuer.yml",
-#     {
-#       "name" = "hpcc-signing-issuer"
-#     }
-#   ))
-# }
-
-# resource "kubernetes_manifest" "signing_cert_issuer" {
-#   manifest = yamldecode(templatefile(
-#     "${path.module}/certificate-issuer.yml",
-#     {
-#       "name"       = "hpcc-signing-issuer"
-#       "secretName" = "hpcc-signing-issuer-key-pair"
-#       "dnsNames"   = var.internal_domain
-#     }
-#   ))
-
-#   depends_on = [kubernetes_manifest.signing_issuer]
-# }
-
-resource "null_resource" "signing_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/signing/issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "signing_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/issuer.yml",
+    {
+      "name" = "hpcc-signing-issuer"
     }
-  }
-  # depends_on = [kubernetes_manifest.local_cert_issuer]
+  ))
 }
 
-resource "null_resource" "signing_cert_issuer" {
-  provisioner "local-exec" {
-    command = <<EOF
-  echo "-------- install kubectl on tfe runner ---------"
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-  kubectl version --client
-  echo "-------- install local ca ------------------"
-  kubectl apply -f ${path.module}/signing/certificate-issuer.yml 
-  echo "------------------------------------------------" 
-  EOF
-    environment = {
-      KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+resource "kubernetes_manifest" "signing_cert_issuer" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/certificate-issuer.yml",
+    {
+      "name"       = "hpcc-signing-issuer"
+      "secretName" = "hpcc-signing-issuer-key-pair"
+      "dnsNames"   = var.internal_domain
     }
-  }
-  depends_on = [null_resource.signing_issuer]
+  ))
+
+  depends_on = [kubernetes_manifest.signing_issuer]
 }
+
+# resource "null_resource" "signing_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/signing/issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   # depends_on = [kubernetes_manifest.local_cert_issuer]
+# }
+
+# resource "null_resource" "signing_cert_issuer" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+#   echo "-------- install kubectl on tfe runner ---------"
+#   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#   kubectl version --client
+#   echo "-------- install local ca ------------------"
+#   kubectl apply -f ${path.module}/signing/certificate-issuer.yml 
+#   echo "------------------------------------------------" 
+#   EOF
+#     environment = {
+#       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#     }
+#   }
+#   depends_on = [null_resource.signing_issuer]
+# }
 
 resource "null_resource" "signing_ca_issuer" {
   provisioner "local-exec" {
@@ -221,7 +222,8 @@ resource "null_resource" "signing_ca_issuer" {
       KUBECONFIG = data.azurerm_kubernetes_cluster.aks.kube_admin_config_raw
     }
   }
-  depends_on = [null_resource.signing_cert_issuer]
+  #depends_on = [null_resource.signing_cert_issuer]
+  depends_on = [kubernetes_manifest.signing_cert_issuer]
 }
 
 ##################public #####################
