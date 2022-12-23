7e55b2f91b643377134927d80ce48ecc009c411f
Sadineni <SadiKa01@risk.regn.net>
2022-12-23 10:07:01 -0800

    adding more than two ports and ciders for testing

--- variables.tf
+++ variables.tf
@@ -1076,9 +1076,9 @@ variable "egress" {
     port     = number ##list(number)
   })
   default = {
-    cidr     = "10.9.8.7/32" ##["10.9.8.7/32"]
+    cidr     = "10.9.8.7/32,  0.0.0.0 0" ##["10.9.8.7/32"]
     protocol = "TCP"
-    port     = 443 ##[443]
+    port     = 443, 65535 ##[443]
   }
   validation {
     condition     = contains(["TCP", "UDP"], var.egress.protocol)
