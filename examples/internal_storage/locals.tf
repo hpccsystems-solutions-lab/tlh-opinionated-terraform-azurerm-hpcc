63f8ab674187bbe0db818fa24567dddc67af468a
Gianchandani <GianMi01@risk.regn.net>
2023-02-21 09:37:22 -0500

    Update examples.tf

--- examples/internal_storage/locals.tf
+++ examples/internal_storage/locals.tf
@@ -1,27 +1,108 @@
 locals {
-  account_code = "us-prctrox-dev"
+  account_code = "us-<productname>-<lifecycle>"
 
-  cluster_name = "us-dev-aks-000"
+  cluster_name = "<aks-cluster-name>"
 
-  cluster_version = "1.21"
+  cluster_version    = "1.23"
 
   dns_resource_group = "app-dns-prod-eastus2"
 
-  internal_domain = "us-prctrox-dev.azure.lnrsg.io"
+  internal_domain = "us-<productname>-<lifecycle>.azure.lnrsg.io"
 
   cluster_name_short = trimprefix(local.cluster_name, "${local.account_code}-")
-  azuread_clusterrole_map = {
-    cluster_admin_users  = {}
-    cluster_view_users   = {}
-    standard_view_users  = {}
-    standard_view_groups = {}
+
+  acr_trusted_ips = {
+    tfe   = "20.69.219.180/32"
+    boca  = "209.243.48.0/20"
+    india = "103.231.79.16/28"
+    ntt   = "83.231.190.16/28"
+    ntt2  = "83.231.235.0/24"
+    uk    = "89.149.148.0/24" # London VPN
+    ala   = "66.241.32.0/19"  # Alpharetta VPN
+    ngd   = "77.67.50.160/28"
+    vpn   = "52.177.80.30/32" # includes vault
   }
 
-  grafana_admin_password  = random_string.random.result
-  smtp_host               = "smtp.lexisnexisrisk.com:25"
-  smtp_from               = "ioa-eks-1@lexisnexisrisk.com"
-  alert_manager_routes    = []
-  alert_manager_recievers = []
+  node_groups = {
+    thorpool = {
+      node_type         = "gpd"
+      node_type_version = "v1"
+      node_size         = "8xlarge"
+      max_capacity      = 309
+      os_config = {
+        sysctl = {
+          net_ipv4_tcp_keepalive_time = 200
+        }
+      }
+      labels = {
+        "lnrs.io/tier" = "standard"
+        "workload"     = "thorpool"
+      }
+    },
+
+    spraypool = {
+      node_type         = "gp"
+      node_type_version = "v1"
+      node_size         = "xlarge"
+      min_capacity      = 3
+      max_capacity      = 3
+      os_config = {
+        sysctl = {
+          net_ipv4_tcp_keepalive_time = 200
+        }
+      }
+      single_group = false
+      labels = {
+        "lnrs.io/tier"  = "standard"
+        "workload"      = "spraypool"
+        "spray-service" = "spraypool"
+      }
+    },
+    servpool = {
+      node_type         = "gpd"
+      node_type_version = "v1"
+      node_size         = "4xlarge"
+      max_capacity      = 9
+      os_config = {
+        sysctl = {
+          net_ipv4_tcp_keepalive_time = 200
+        }
+      }
+      labels = {
+        "lnrs.io/tier" = "standard"
+        "workload"     = "servpool"
+      }
+    }
+  }
+
+  internal_ips = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  firewall_ip  = "<your-firewall-hub-ip>"
+
+  routes = merge(
+    {
+      internet = {
+        address_prefix = "0.0.0.0/0"
+        next_hop_type  = "Internet"
+      }
+      local-vnet = {
+        address_prefix = var.cidr_block
+        next_hop_type  = "VnetLocal"
+      }
+    },
+    { for ip in local.internal_ips :
+      format("internal-%s", index(local.internal_ips, ip) + 1) => {
+        address_prefix         = ip
+        next_hop_type          = "VirtualAppliance"
+        next_hop_in_ip_address = local.firewall_ip
+      }
+  })
+
+  route_table = {
+    routes                        = local.routes
+    disable_bgp_route_propagation = true
+    use_inline_routes             = false
+  }
+
 
   k8s_exec_auth_env = {
     AAD_SERVICE_PRINCIPAL_CLIENT_ID     = module.azure_credentials.client_id
@@ -34,18 +115,8 @@ locals {
     AZURE_CLIENT_ID       = module.azure_credentials.client_id
     AZURE_CLIENT_SECRET   = module.azure_credentials.client_secret
   }
+
   admin_group_object_ids = [var.aad_group_id]
 
-  acr_trusted_ips = {
-    tfe   = "20.69.219.180/32"
-    boca  = "209.243.48.0/20"
-    india = "103.231.79.16/28"
-    ntt   = "83.231.190.16/28"
-    ntt2  = "83.231.235.0/24"
-    uk    = "89.149.148.0/24" # London VPN
-    ala   = "66.241.32.0/19"  # Alpharetta VPN
-    ngd   = "77.67.50.160/28"
-    vpn   = "52.177.80.30/32" # includes vault
-    vault = "52.138.106.19/32"
-  }
+
 }
