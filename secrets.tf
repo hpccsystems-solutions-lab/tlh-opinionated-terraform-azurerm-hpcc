7b6daec075d061d3bc9b3d64e0cb7430b15c868c
Gianchandani <GianMi01@risk.regn.net>
2023-02-21 15:05:22 -0500

    ESP Approle Secret

--- secrets.tf
+++ secrets.tf
@@ -60,3 +60,25 @@ resource "kubernetes_secret" "ecluser_approle_secret_id" {
   type = "Opaque"
 }
 
+resource "kubernetes_secret" "esp_approle_secret_id" {
+  depends_on = [
+    kubernetes_namespace.default
+  ]
+
+  for_each = local.vault_enabled && var.vault_secrets.esp_approle_secret != null ? var.vault_secrets.esp_approle_secret : {}
+
+
+  metadata {
+    name      = each.value.secret_name
+    namespace = var.namespace.name
+    labels = {
+      name = each.value.secret_name
+    }
+  }
+  data = {
+    secret-id = each.value.secret_value
+  }
+
+  type = "Opaque"
+}
+
