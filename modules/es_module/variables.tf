5f786b86a31b067cea7ae7a9d7ba99172a3cb4ee
Gianchandani <gianmi01@HBEGIANMI01.Risk.regn.net>
2023-05-01 13:02:55 -0400

    No Secret Stores for now

--- modules/es_module/variables.tf
+++ modules/es_module/variables.tf
@@ -16,18 +16,18 @@ variable "vault_secret_id" {
   })
 }
 
-variable "secret_stores" {
-  description = "A map of SecretStore names and their Vault Configuration"
-  type = map(object({
-    secret_store_name      = string
-    secret_store_namespace = string
-    vault_url              = string
-    vault_namespace        = string
-    kv_path                = string
-    approle_role_id        = string
-    approle_secret_id_name = string
-  }))
-}
+# variable "secret_stores" {
+#   description = "A map of SecretStore names and their Vault Configuration"
+#   type = map(object({
+#     secret_store_name      = string
+#     secret_store_namespace = string
+#     vault_url              = string
+#     vault_namespace        = string
+#     kv_path                = string
+#     approle_role_id        = string
+#     approle_secret_id_name = string
+#   }))
+# }
 
 # variable "ext_secret" {
 #   decrdescription = "A map of ExternalSecret names and their Vault Configuration""  
