936cc1c3655e52d9e1b1c5e72b239495a523ecda
Gianchandani <gianmi01@HBEGIANMI01.Risk.regn.net>
2023-05-10 10:31:25 -0400

    Fix Role Name

--- log_access.tf
+++ log_access.tf
@@ -3,6 +3,6 @@
 resource "azurerm_role_assignment" "log_access_subscription" {
 
   scope                = coalesce(var.log_access_role_assignment.scope, data.azurerm_subscription.current.id)
-  role_definition_name = "Log-Analytics-Contributor"
+  role_definition_name = "Log Analytics Contributor"
   principal_id         = var.log_access_role_assignment.object_id
 }
