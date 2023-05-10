734f1ad9d5d9835afd0792d0cb721c8b833d89dd
Gianchandani <gianmi01@HBEGIANMI01.Risk.regn.net>
2023-05-10 10:01:00 -0400

    Update Display Name for Subscription

--- log_access.tf
+++ log_access.tf
@@ -2,7 +2,7 @@
 
 resource "azurerm_role_assignment" "log_access_subscription" {
 
-  scope                = coalesce(var.log_access_role_assignment.scope, data.azurerm_subscription.current.subscription_id)
-  role_definition_name = format("%s-%s-Log-Analytics-Contributor", local.aks_trimmed_name, data.azurerm_subscription.current)
+  scope                = coalesce(var.log_access_role_assignment.scope, data.azurerm_subscription.current.id)
+  role_definition_name = format("%s-%s-Log-Analytics-Contributor", local.aks_trimmed_name, data.azurerm_subscription.current.display_name) 
   principal_id         = var.log_access_role_assignment.object_id
 }
