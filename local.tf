97bc29d5ef17cd4e76b110e48c2012e6e19ffccc
Actions <action@github.com>
2023-11-27 16:28:27 +0000

    Terraform Auto-Format

--- local.tf
+++ local.tf
@@ -662,7 +662,7 @@ locals {
       }
       busybox = local.acr_default.busybox
       image = merge({
-        version    = var.hpcc_container.version
+        version = var.hpcc_container.version
         #version    = var.hpcc_version
         root       = var.hpcc_container.image_root
         name       = var.hpcc_container.image_name
